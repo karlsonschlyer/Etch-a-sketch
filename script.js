748bc29be088c609ad0cc47ea2f9f662b0261cab
Karlson Schlyer <42825291+karlsonschlyer@users.noreply.github.com>
2023-11-28 22:39:33 -0600

    Give classes to divs during creation and move <script>

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const rowAmount = 16
 const boxAmount = 16
 
 for (let i = 0; i < rowAmount; i++) {
-    const divRow = document.createElement('div');
+    let divRow = document.createElement('div');
+    divRow.setAttribute('class', 'row');
     for (let i = 0; i < boxAmount; i++) {
-        const divBox = document.createElement('div');
+        let divBox = document.createElement('div');
+        divBox.setAttribute('class', 'box');
         divRow.appendChild(divBox);
     }
+    divContainer.appendChild(divRow);
 }
