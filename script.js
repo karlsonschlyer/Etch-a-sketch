72197b1e205a01ac83518e95c402db6816e64da0
Karlson Schlyer <42825291+karlsonschlyer@users.noreply.github.com>
2023-11-30 19:24:35 -0600

    Add function removeAllChildNodes and drawGrid, add button to html, hookup button to functions

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const divContainer = document.querySelector('#container');
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
-const rowAmount = 16
-const boxAmount = 16
-
-for (let i = 0; i < rowAmount; i++) {
+function drawGrid (size) {
+    const rowAmount = size;
+    const boxAmount = size;
+    for (let i = 0; i < rowAmount; i++) {
     let divRow = document.createElement('div');
     divRow.setAttribute('class', 'row');
     for (let i = 0; i < boxAmount; i++) {
         let divBox = document.createElement('div');
         divBox.setAttribute('class', 'box');
+        divBox.addEventListener('mouseenter',(event) => {
+            divBox.setAttribute('class', 'box change-color');
+        })
         divRow.appendChild(divBox);
     }
     divContainer.appendChild(divRow);
+    }
 }
+
+const divContainer = document.querySelector('#container');
+const button = document.querySelector('button');
+button.addEventListener('click',(event) => {
+    removeAllChildNodes(divContainer);
+    const size = prompt("Enter a number from 1 to 100");
+    drawGrid(size);
+})
+
